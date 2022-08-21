63d155809455540861789d6b8ba20e19b9d7c41a
chrisislearning <chris.achinga@student.moringaschool.com>
2022-08-21 23:47:04 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const p = document.querySelector("#text");
+    p.innerHTML = "This is really cool!";
+  });
